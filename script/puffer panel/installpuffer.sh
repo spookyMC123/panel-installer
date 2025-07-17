f507e790d28ce219da1443f79b5730271bb32c27
spookyMC123 <editormaster505@gmail.com>
2025-07-17 18:44:02 +0600

    installpuffer.sh

---
+++ script/puffer panel/installpuffer.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Define colors
+GREEN="\e[32m"
+YELLOW="\e[33m"
+CYAN="\e[36m"
+RED="\e[31m"
+RESET="\e[0m"
+
+# Clear the terminal
+clear
+
+# Display Installer Name
+echo -e "${CYAN}==============================${RESET}"
+echo -e "${CYAN}      Skyport-Installer       ${RESET}"
+echo -e "${CYAN}==============================${RESET}"
+
+# Menu options
+echo -e "${YELLOW}Select an option:${RESET}"
+echo "1) Install pufferpanel on github"
+echo "2) Install pufferpanel on all vps and codesandbox"
+echo -n "Enter your choice: "
+read -r choice
+
+# Run the selected installation script
+case $choice in
+    1)
+        echo -e "${GREEN}Installing github pufferPanel...${RESET}"
+        bash <(curl -fsSL https://github.com/spookyMC123/panel-installer/raw/refs/heads/main/script/puffer%20panel/git-install.sh)
+        ;;
+    2)
+        echo -e "${GREEN}Installing vps pufferpanel...${RESET}"
+        bash <(curl -fsSL https://github.com/spookyMC123/panel-installer/raw/refs/heads/main/script/puffer%20panel/codesandboxvpspufferpanelinstall.sh)
+        ;;
+   
+    *)
+        echo -e "${RED}Invalid choice! Please run the script again.${RESET}"
+        exit 1
+        ;;
+esac
+
+echo -e "${GREEN}puffer panel process completed successfully!${RESET}"
