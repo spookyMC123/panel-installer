b22eec589ae973f25712c2f1cbd30505491781f5
spookyMC123 <editormaster505@gmail.com>
2025-07-17 18:37:07 +0600

    git-install.sh

--- script/puffer panel/git-install.sh
+++ script/puffer panel/git-install.sh
@@ -1,10 +1,12 @@
+sudo su
 sudo apt install systemctl
-
 curl -s https://packagecloud.io/install/repositories/pufferpanel/pufferpanel/script.deb.sh?any=true | sudo bash
 sudo apt update
 sudo apt-get install pufferpanel
 
 sudo su
 sudo pufferpanel user add
+nohup pufferpanel run > panel.log 2>&1 &
 
+echo "use http tunnel for view panel beause github not support hostin on Codespace"
 echo "then run pufferpanel run then run panel at 8080 port"
