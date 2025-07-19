cde5fcc75871425fec4e98cae2032e732ca568f6
spookyMC123 <editormaster505@gmail.com>
2025-07-19 22:35:53 +0600

    Update install.sh

--- script/ptrodactyl/install.sh
+++ script/ptrodactyl/install.sh
@@ -6,20 +6,29 @@ YELLOW="\e[33m"
 CYAN="\e[36m"
 RED="\e[31m"
 RESET="\e[0m"
+BG_BLACK="\e[40m"
+BG_RED="\e[41m"
+BG_GREEN="\e[42m"
+BG_YELLOW="\e[43m"
+BG_BLUE="\e[44m"
+BG_MAGENTA="\e[45m"
+BG_CYAN="\e[46m"
+BG_WHITE="\e[47m"
+
 
 # Clear the terminal
 clear
 
 # Display Installer Name
 echo -e "${CYAN}==============================${RESET}"
-echo -e "${CYAN}      Skyport-Installer       ${RESET}"
+echo -e "${CYAN}      ptero-Installer       ${RESET}"
 echo -e "${CYAN}==============================${RESET}"
 
 
 # Menu options
 echo -e "${YELLOW}Select an option:${RESET}"
-echo -e "${GREEN}1)${RESET} Install pterodactl (on vps using Ipv4/with Wings)"
-echo -e "${GREEN}2)${RESET} Install pterodactl 2 (on github without Ipv4)"
+echo -e "${BG_BLUE}1)${RESET} Install pterodactl (on vps using Ipv4/with Wings)"
+echo -e "${BG_BLUE}2)${RESET} Install pterodactl 2 (on github without Ipv4)"
 echo -ne "${YELLOW}Enter your choice: ${RESET}"
 read -r choice
 
