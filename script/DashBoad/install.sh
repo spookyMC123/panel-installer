d48dee65f26bf9e8be71bed085db20336fcbe00b
spookyMC123 <editormaster505@gmail.com>
2025-07-19 23:44:15 +0600

    Update install.sh

--- script/DashBoad/install.sh
+++ script/DashBoad/install.sh
@@ -19,18 +19,23 @@ echo -e "${CYAN}==============================${RESET}"
 echo -e "${YELLOW}Select an option:${RESET}"
 echo "1) Install Oversee"
 echo "2) Install Draco DashBoad's"
+echo "0) back to panel-installer"
 echo -n "Enter your choice: "
 read -r choice
 
 # Run the selected installation script
 case $choice in
     1)
-        echo -e "${GREEN}Installing Skyport Panel...${RESET}"
-        bash <(curl -fsSL https://raw.githubusercontent.com/Mehetab1234/Skyport-installer/main/scripts/panel.sh)
+        echo -e "${GREEN}Installing Oversee Dash...${RESET}"
+        bash <(curl -fsSL https://github.com/spookyMC123/panel-installer/raw/refs/heads/main/script/DashBoad/overseeinstall.sh)
         ;;
     2)
-        echo -e "${GREEN}Installing Wings...${RESET}"
+        echo -e "${GREEN}Installing Draco dash...${RESET}"
         bash <(curl -fsSL https://raw.githubusercontent.com/Mehetab1234/Skyport-installer/main/scripts/node.sh)
+        ;;
+     0)
+        echo -e "${GREEN}Backing...${RESET}"
+        bash <(curl -fsSL https://github.com/spookyMC123/panel-installer/raw/refs/heads/main/installer.sh)
         ;;
 
     *)
